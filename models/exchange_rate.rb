1688560a74c4d67e16a99a6e5323296736423b02
Russell Williams <russellwilliamscc@gmail.com>
2016-10-18 00:23:53 +0100

    added select and amount to the form

--- models/exchange_rate.rb
+++ models/exchange_rate.rb
@@ -3,8 +3,12 @@ require ('httparty')
 
 class ExchangeRate
 
+  def initialize
+    received = HTTParty.get("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml")
+    @data = received["Envelope"]["Cube"]["Cube"]
+  end
+  
   def parse_data()
-    @data = HTTParty.get("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml")
     return @data
   end
 
