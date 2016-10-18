ccfecee108e45eee8fb3906552367746b2acffd1
Russell Williams <russellwilliamscc@gmail.com>
2016-10-18 01:23:19 +0100

    test not working as, 0 runs

--- specs/exchange_rate_specs.rb
+++ specs/exchange_rate_specs.rb
@@ -4,7 +4,7 @@ require_relative('../models/exchange_rate')
 class TestExchangeRate < MiniTest::Test
 
   def setup
-    @exchange_rate = ExchangeRate.new(5, 1, 2)
+    @exchange_rate = ExchangeRate.new(0, 5, 1, 2)
     assert_equal(10, @exchange_rate.at())
   end
 
