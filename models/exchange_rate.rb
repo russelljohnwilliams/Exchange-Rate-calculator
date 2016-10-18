398e7ec512899ce34bc52dd9b5f946803f41227f
Russell Williams <russellwilliamscc@gmail.com>
2016-10-18 01:09:35 +0100

    refactored get_index_of_from_ccy and get_index_of_to_ccy

--- models/exchange_rate.rb
+++ models/exchange_rate.rb
@@ -19,16 +19,25 @@ class ExchangeRate
     return @data
   end
 
-  def exchange_rate()
+  def get_index_of_from_ccy()
     cube = @data[@i]["Cube"]
-
     find_from_index = cube.find{ |element| element["currency"] == @from_ccy}
     x = cube.index(find_from_index)
     from_amount = cube[x]["rate"].to_f
+    return from_amount
+  end
 
+  def get_index_of_to_ccy()
+    cube = @data[@i]["Cube"]
     find_to_index = cube.find{ |element| element["currency"] == @to_ccy}
     y = cube.index(find_to_index)
     to_amount = cube[y]["rate"].to_f
+    return to_amount
+  end
+
+  def exchange_rate()
+    from_amount = self.get_index_of_from_ccy()
+    to_amount = self.get_index_of_to_ccy()
 
     return "index number: ", @i, "amount to exchange: ", @amount, "currency & rate to exchanghe from: ", @from_ccy, from_amount, "currency & rate to exchanghe to: ", @to_ccy, to_amount
   end
